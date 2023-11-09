17fffbb631579c81bf847c213a5448ba039c27fa
Nick Hanson <hansonnick25@gmail.com>
2023-11-08 22:21:09 -0600

    Updated iframe to use Box and fixed styling

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,19 +1,41 @@
-import { Typography } from '@mui/material'
+import {
+  Box,
+  Typography,
+  Card,
+  CardHeader,
+  Grid,
+  CardMedia,
+  CardContent,
+  Button,
+  Link,
+} from '@mui/material'
+import resumeFile from '../assets/NickHansonResume.pdf'
 
 const Resume = () => {
   return (
-    <div className="resume" id="resume">
+    <Box className="contact-me" id="portfolio">
       <Typography variant="h4" margin={3} textAlign={'center'}>
         Resume
       </Typography>
-      <iframe
-        src="/assets/NickHansonResume.docx"
-        title="Nick Hanson's Resume"
-        height={800}
-        width={'80%'}
-        style={{ margin: 'auto', display: 'block' }}
-      ></iframe>
-    </div>
+      <Grid container justifyContent={'center'}>
+        <Grid item sm={10} md={10} lg={9} textAlign={'center'}>
+          <Card>
+            <CardHeader subheader="Download Below!" />
+            <CardMedia component="iframe" src={resumeFile} height={800} />
+            <CardContent>
+              <Link
+                href={resumeFile}
+                download="NickHansonResume"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Button component="a">Download</Button>
+              </Link>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
