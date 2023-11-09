852764643cb803d0a248f020962adacde8d12302
Nick Hanson <hansonnick25@gmail.com>
2023-11-08 22:26:03 -0600

    Add loading button, response, styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,20 +1,21 @@
 import {
   Box,
-  Button,
   Card,
   CardContent,
-  CardHeader,
   Grid,
   TextField,
   Typography,
 } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send'
+import LoadingButton from '@mui/lab/LoadingButton'
 import { useState } from 'react'
 import emailjs from '@emailjs/browser'
 import { validateEmail } from '../utils/helpers'
 
 const Contact = () => {
   const [formData, setFormData] = useState({ email: '', name: '', message: '' })
-  const [errorMessage, setErrorMessage] = useState('')
+  const [responseMessage, setResponseMessage] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const handleChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
@@ -22,110 +23,105 @@ const Contact = () => {
 
   const handleSubmit = e => {
     e.preventDefault()
-    let form = document.getElementById('contact-me-form')
-    console.log(formData)
+    // let form = document.getElementById('contact-me-form')
 
     if (!formData.email || !formData.message) {
-      setErrorMessage('Please fill out all fields')
+      setResponseMessage('Please fill out all fields')
+      return
+    }
+    if (!validateEmail(formData.email)) {
+      setResponseMessage('Please enter a valid email address')
       return
     }
 
-    if (!validateEmail(formData.email)) {
-      setErrorMessage('Please enter a valid email address')
-      return
-    }
+    setLoading(true)
+
     emailjs
       .sendForm(
         'service_7pb3op9',
         'template_6rlkit6',
-        form,
+        document.getElementById('contact-me-form'),
         'qqvh9l_k9DDGWWdkf'
       )
       .then(
         result => {
           console.log(result.text)
-          alert('Message sent successfully')
+          setResponseMessage('Message sent successfully')
+          setLoading(false)
         },
         error => {
           console.log(error.text)
         }
       )
-
-    formData.email = ''
-    formData.name = ''
-    formData.message = ''
   }
 
   return (
-    <Grid container justifyContent={'center'}>
-      <Grid item sm={10} md={10} lg={8} textAlign={'center'}>
-        <Card
-          sx={{
-            borderRadius: '20px',
-          }}
-        >
-          <CardHeader
-            title="Contact Me"
-            sx={{
-              textAlign: 'center',
-            }}
-          />
-          <CardContent>
-            <form id="contact-me-form">
-              <Box paddingLeft={4} paddingRight={4}>
-                <Typography variant="body">
-                  Please feel free to reach out to me with any questions or
-                  inquiries you may have! I will get back to you as soon as I am
-                  able.
-                </Typography>
-              </Box>
-              <Box
-                display="flex"
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-                margin={3}
+    <Box className="contact-me" id="portfolio">
+      <Typography variant="h4" margin={3} textAlign={'center'}>
+        Contact Me
+      </Typography>
+      <Grid container justifyContent={'center'}>
+        <Grid item sm={10} md={10} lg={7} textAlign={'center'}>
+          <Card>
+            <CardContent>
+              <form id="contact-me-form">
+                <Box px={5}>
+                  <Typography variant="body">
+                    Please feel free to reach out to me with any questions or
+                    inquiries you may have! I will get back to you as soon as I
+                    am able.
+                  </Typography>
+                </Box>
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  justifyContent="center"
+                  alignItems="center"
+                  margin={3}
+                >
+                  <TextField
+                    id="name-form"
+                    label="Your Name"
+                    name="name"
+                    onChange={handleChange}
+                    margin="dense"
+                    fullWidth
+                  />
+                  <TextField
+                    id="email-form"
+                    label="Your Email"
+                    name="email"
+                    onChange={handleChange}
+                    margin="dense"
+                    fullWidth
+                  />
+                  <TextField
+                    id="message-form"
+                    label="Your Message Here"
+                    multiline
+                    rows={4}
+                    name="message"
+                    onChange={handleChange}
+                    margin="dense"
+                    fullWidth
+                  />
+                </Box>
+                <Typography>{responseMessage}</Typography>
+              </form>
+              <LoadingButton
+                onClick={handleSubmit}
+                endIcon={<SendIcon />}
+                loading={loading}
+                loadingPosition="end"
+                variant="contained"
               >
-                <TextField
-                  required
-                  id="name-form"
-                  label="Your Name"
-                  variant="filled"
-                  name="name"
-                  onChange={handleChange}
-                  margin="dense"
-                  fullWidth
-                />
-                <TextField
-                  id="email-form"
-                  label="Your Email"
-                  variant="filled"
-                  name="email"
-                  onChange={handleChange}
-                  margin="dense"
-                  fullWidth
-                />
-                <TextField
-                  id="message-form"
-                  label="Your Message Here"
-                  multiline
-                  rows={4}
-                  variant="filled"
-                  name="message"
-                  onChange={handleChange}
-                  margin="dense"
-                  fullWidth
-                />
-              </Box>
-              {errorMessage && <p className="mt-2">{errorMessage}</p>}
-            </form>
-            <Button variant="contained" onClick={handleSubmit}>
-              <Typography variant="body">Submit</Typography>
-            </Button>
-          </CardContent>
-        </Card>
+                <span>Send</span>
+              </LoadingButton>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   )
 }
 
